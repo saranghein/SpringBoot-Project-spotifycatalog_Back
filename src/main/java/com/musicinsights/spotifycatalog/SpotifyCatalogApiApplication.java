e921e2433ff38286a45c735d0c56a9d7a644eafc
saranghein <saranghein@gmail.com>
2025-12-17 14:07:07 +0900

    :broom: Chore: 프로젝트 파일 추가

---
+++ src/main/java/com/musicinsights/spotifycatalog/SpotifyCatalogApiApplication.java
@@ -0,0 +1,13 @@
+package com.musicinsights.spotifycatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyCatalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyCatalogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musicinsights/spotifycatalog/SpotifyCatalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicinsights.spotifycatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyCatalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
