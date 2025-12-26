ee2ebe1985146def4e06b0421868fddd2d7b9de2
github-action-benchmark <github@users.noreply.github.com>
2025-12-26 06:21:36 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for d06df26613d7324f0625cf815d50576ea8992cbd

---
+++ dev/bench-memory/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1766730096200,
+  "repoUrl": "https://github.com/saranghein/SpringBoot-Project-spotifycatalog_Back",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "98319061+saranghein@users.noreply.github.com",
+            "name": "LeeHaeIn",
+            "username": "saranghein"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d06df26613d7324f0625cf815d50576ea8992cbd",
+          "message": "Fix path to benchmark result JSON in workflow",
+          "timestamp": "2025-12-26T15:18:44+09:00",
+          "tree_id": "11e60974664cf93c5db58851459f3dcc3a5d4d0f",
+          "url": "https://github.com/saranghein/SpringBoot-Project-spotifycatalog_Back/commit/d06df26613d7324f0625cf815d50576ea8992cbd"
+        },
+        "date": 1766730095959,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "gradle test peak WorkingSet (Windows)",
+            "value": 564.9,
+            "unit": "MB"
+          }
+        ]
+      }
+    ]
+  }
+}
