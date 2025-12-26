74e9f88c50d458ce9d2bddb5998c4e58c7a9c5f4
github-action-benchmark <github@users.noreply.github.com>
2025-12-26 06:26:02 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 548f6d92a8601fc2933d3ea7375cfe1c545e8f57

--- dev/bench-memory/data.js
+++ dev/bench-memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1766730096200,
+  "lastUpdate": 1766730362689,
   "repoUrl": "https://github.com/saranghein/SpringBoot-Project-spotifycatalog_Back",
   "entries": {
     "Benchmark": [
@@ -23,6 +23,35 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/saranghein/SpringBoot-Project-spotifycatalog_Back/commit/d06df26613d7324f0625cf815d50576ea8992cbd"
         },
         "date": 1766730095959,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "gradle test peak WorkingSet (Windows)",
+            "value": 564.9,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "98319061+saranghein@users.noreply.github.com",
+            "name": "LeeHaeIn",
+            "username": "saranghein"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "548f6d92a8601fc2933d3ea7375cfe1c545e8f57",
+          "message": "Update GitHub Actions workflow for benchmarks",
+          "timestamp": "2025-12-26T15:25:51+09:00",
+          "tree_id": "8ff4f662c87b6a5b7f3d02449d4d629e2d24a242",
+          "url": "https://github.com/saranghein/SpringBoot-Project-spotifycatalog_Back/commit/548f6d92a8601fc2933d3ea7375cfe1c545e8f57"
+        },
+        "date": 1766730361764,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
